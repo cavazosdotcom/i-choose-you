426f9887143bf15e25c2edde850cf144206f508e
cmseibel <cam.m.seib@gmail.com>
2022-04-13 19:42:04 -0700

    Write initial Pokemon model.

--- server/models/Pokemon.js
+++ server/models/Pokemon.js
@@ -1,7 +1,23 @@
 const { Schema, model } = require('mongoose');
 
-const pokeSchema = new Schema({
+const moveSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    }
+});
 
+const pokeSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    type: [String],
+    moves: [moveSchema]
 });
 
 const Pokemon = model("Pokemon", pokeSchema);
