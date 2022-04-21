d59f4ccc39169edcf71fb056df5f8aec2ac1340f
Luke <lukeconrad.stew@gmail.com>
2022-04-20 20:23:05 -0700

    searh-bar branch, with filtered name search in home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,14 @@
+//import React, { useState } from 'react';
+// import ImageOne from '../images/1st.png';
+// import ImageTwo from '../images/2nd.png';
+// import ImageThree from '../images/3rd.png';
+
+
+
 const Home = () => {
+// const [name, setName] = useState('');
+// const pokemon = [{name:'John', image:ImageOne, description: "johns image"}, {name:'Bob', image:ImageTwo, description: "Bibs image"}, {name:'Chris', image:ImageThree, description: "  chris image"} ]
+
 
   return (
     <main>
@@ -7,10 +17,23 @@ const Home = () => {
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: '1px dotted #1a1a1a' }}
         >
-          Content Here
-        </div>
+        {/* <input value={name} onChange={(e) => setName(e.target.value)}>
+        
+        </input> */}
+        {/* </div>
         <div className="col-12 col-md-8 mb-3">
-          Content Here
+          {pokemon.filter(pok => pok.name.toLowerCase().includes (name.toLowerCase()) ).map(pok => (
+            <div>
+            <p>
+              {pok.name}
+            </p>
+            <img src={pok.image}>
+            </img>
+            <p>
+              {pok.description}
+            </p>
+            </div>
+          ))} */}
         </div>
       </div>
     </main>
