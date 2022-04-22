519aba4342ea9d726320b6f92bb8870f520eeb9d
cmseibel <cam.m.seib@gmail.com>
2022-04-22 13:13:17 -0700

    Pass addPokemon onclick as prop

--- client/src/components/PokemonCard/index.js
+++ client/src/components/PokemonCard/index.js
@@ -5,11 +5,10 @@ import { ADD_POKEMON } from '../../utils/mutations';
 
 // import { Link } from 'react-router-dom';
 
-const PokemonCard = ({pokemon, teamName}) => {
+const PokemonCard = ({pokemon, teamName, addPokemon}) => {
     const expression = /\d+/g;
     const [, dexNumber] = pokemon.url.match(expression);
     // console.log(dexNumber);
-    const [addPokemon, { error }] = useMutation(ADD_POKEMON);
     
 
     // const [teamName, setTeamName] = useState();
@@ -25,7 +24,9 @@ const PokemonCard = ({pokemon, teamName}) => {
             <div className="d-inline-flex">
                 <div className="card-body bg-danger text-white text-center wide">
                     <p>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</p>
-                    <button className="btn btn-primary" onClick={() => addPokemon({variables: {teamName: teamName, pokeName:pokemon.name, dexNumber: parseInt(dexNumber)}})}>+</button>
+                    <button className="btn btn-primary" onClick={() => {
+                        addPokemon({variables: {teamName: teamName, pokeName:pokemon.name, dexNumber: parseInt(dexNumber)}});
+                    }}>+</button>
                 </div>
                 <div className="card-body p-0">
                     <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${dexNumber}.png`} className="wide" alt={`A small sprite of ${pokemon.name}`}/>
