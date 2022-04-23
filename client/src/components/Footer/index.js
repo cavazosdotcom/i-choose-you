35498e19c637525356eee69131afa5fd063442a6
Marshall Baccari <mbaccari@uw.edu>
2022-04-23 16:13:52 -0700

    created landing page for those not logged in. still trying to figure out layout

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
+    <footer className="w-100 mt-auto bg-secondary p-4 fixed-bottom">
       Thank you for visiting I Choose You, is your team ready for to enter to battle!
     </footer>
   );
